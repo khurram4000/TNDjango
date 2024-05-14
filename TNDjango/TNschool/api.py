3bd3ce72cb44f26f9812515b8cafced7bdf85aee
khurram4000 <107985762+khurram4000@users.noreply.github.com>
2024-05-14 16:28:42 +0500

    Add files via upload

---
+++ TNDjango/TNschool/api.py
@@ -0,0 +1,4 @@
+BASE_URL = 'http://157.245.4.4:8080/'
+LOGIN_END_POINT = 'auth/login/'
+GET_ALL_ASSIGNED_COURSES_END_POINT = 'courses/get-all-assigned-course/'
+GET_COURSE_LECTURES_END_POINT = 'courses/get-course-lectures/'
